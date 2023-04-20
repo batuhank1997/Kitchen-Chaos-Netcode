4792233501197bd9b4c784285c0bf0434fbbc851
Batuhan <batuhank1997@hotmail.com>
2023-04-20 12:34:38 +0300

    add new input system for player

--- Assets/_Scripts/Game/Player/Player.cs
+++ Assets/_Scripts/Game/Player/Player.cs
@@ -7,39 +7,27 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float playerSpeed;
     [SerializeField] private float rotationSpeed;
+    [SerializeField] private GameInput gameInput;
 
-    public bool IsWalking { get; set; }
+    public bool IsWalking { get; private set; }
 
     private void Update()
     {
+        Move();
+    }
+
+    void Move()
+    {
         var deltaTime = Time.deltaTime;
         
-        Vector2 inputVector = Vector2.zero;
-        
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         var moveDir = new Vector3(inputVector.x, 0, inputVector.y);
 
         IsWalking = moveDir != Vector3.zero;
         
         transform.position += moveDir * (playerSpeed * deltaTime);
+        transform.position = Vector3.Slerp(transform.position, transform.position + moveDir * (playerSpeed * deltaTime), 0.075f);
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotationSpeed * deltaTime);
     }
 }
