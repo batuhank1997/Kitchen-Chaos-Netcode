d2cc9ff51c765c0bd1f1926e03c9cc656a09e656
Batuhan <batuhank1997@hotmail.com>
2023-04-20 22:36:01 +0300

    Kitchen scripts cleaned

--- Assets/_Dev/_Scripts/Game/Player/PlayerInteractions.cs
+++ Assets/_Dev/_Scripts/Game/Player/PlayerInteractions.cs
@@ -6,11 +6,7 @@ using UnityEngine;
 public class PlayerInteractions : MonoBehaviour
 {
     [SerializeField] private PlayerController playerController;
-    
-    public Action<ClearCounter> OnSelectedCounterChanged;
-    
     private Vector3 lastInteractDir;
-    private ClearCounter selectedCounter;
     
     private void Start()
     {
@@ -19,66 +15,18 @@ public class PlayerInteractions : MonoBehaviour
 
     private void OnInteractAction()
     {
-        if (!selectedCounter) return;
-        
         Vector2 inputVector = playerController.GameInput.GetMovementVectorNormalized();
 
         var moveDir = new Vector3(inputVector.x, 0, inputVector.y);
 
         if (moveDir != Vector3.zero)
-        {
             lastInteractDir = moveDir;
-        }
 
         var interactDistance = 2f;
 
         if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit hitInfo, interactDistance)) 
         {
-            if (hitInfo.transform.TryGetComponent(out ClearCounter clearCounter))
-            {
-                clearCounter.Interact();
-            }
+            Debug.Log(hitInfo.collider.name);
         }
-    }
-
-    void Update()
-    {
-        HandleInteractions();
-    }
-    
-    void HandleInteractions()
-    {
-        Vector2 inputVector = playerController.GameInput.GetMovementVectorNormalized();
-
-        var moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-
-        if (moveDir != Vector3.zero)
-        {
-            lastInteractDir = moveDir;
-        }
-
-        var interactDistance = 2f;
-
-        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit hitInfo, interactDistance)) 
-        {
-            if (hitInfo.transform.TryGetComponent(out ClearCounter clearCounter))
-            {
-                if (clearCounter != selectedCounter)
-                    SetSelectedCounter(clearCounter);
-                
-            }
-            else
-            {
-                SetSelectedCounter(null);
-            }
-        }
-        else
-            SetSelectedCounter(null);
-    }
-
-    void SetSelectedCounter(ClearCounter clearCounter)
-    {
-        selectedCounter = clearCounter;
-        OnSelectedCounterChanged?.Invoke(selectedCounter);
     }
 }
